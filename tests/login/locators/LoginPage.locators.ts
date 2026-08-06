cfcd6a1b2e2f79dbd10e89da080722330880032a
Sparsh <sparsh13h@gmail.com>
2026-08-06 10:58:11 +0530

    Remove legacy tests/login files after consolidation into tests/saucedemo

--- tests/login/locators/LoginPage.locators.ts
+++
@@ -1,6 +0,0 @@
-export class LoginPageLocators {
-  static readonly usernameInput = 'input[data-test="username"]';
-  static readonly passwordInput = 'input[data-test="password"]';
-  static readonly loginButton = 'input[data-test="login-button"]';
-  static readonly errorMessage = '[data-test="error"]';
-}
